069955941a44d6bdf23f94aa9b0e6f9aad2c931d
pradyumansingh989 <66694735+pradyumansingh989@users.noreply.github.com>
2020-10-13 10:54:17 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -8,15 +8,31 @@ const PORT = process.env.PORT || 8000
 // Assets
 app.use(express.static('public'))
 
-app.get('/', function(req,res){
-    res.render('home');
-})
-
 //set template
 
 app.use(expressLayout);
 app.set('views', path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+
+
+
+app.get('/', function(req,res){
+    res.render('home');
+})
+
+app.get('/cart',function(req,res){
+    res.render('customers/cart');
+})
+
+app.get('/login',function(req,res){
+    res.render('auth/login');
+})
+
+app.get('/register',function(req,res){
+    res.render('auth/register');
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is Up and Running on port ${PORT}`);
 });
