95f7880407a64a7c9b3f92041ed7b481eeab5757
pradyumansingh989 <66694735+pradyumansingh989@users.noreply.github.com>
2020-10-17 20:20:27 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-console.log('hello from app.js');
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(laptops){
+    axios.post('/update-cart',laptops).then(res =>{
+        cartCounter.innerText = res.data.totalQty
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+            progressBar: false
+        }).show();
+    }).catch(err =>{
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+            progressBar: false
+        }).show();
+    })
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click',(e) => {
+        let laptops = JSON.parse(btn.dataset.laptops)
+        updateCart(laptops)
+    })
+});
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
@@ -5,6 +6,9 @@ const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 8000
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo')(session); //passing session
 
 
 //Database connection
@@ -17,9 +21,30 @@ connection.once('open',()=>{
     console.log('Connection failed..')
 });
 
+//session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+//session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized:false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24} //24hrs
+}))
 
+app.use(flash());
 // Assets
 app.use(express.static('public'))
+app.use(express.json())
+
+// Global Middleware
+app.use((req,res,next)=>{
+    res.locals.session = req.session
+    next()
+})
 
 //set template
 
